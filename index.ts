31c11da578a2f2a19bdbcf1c8b944206a194f80b
mkgupta911 <mkgupta911@gmail.com>
2021-10-14 09:45:58 +0530

    4 files modified

--- index.ts
+++ index.ts
@@ -1,6 +1,27 @@
 // Import stylesheets
 import './style.css';
 
-// Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+import "@grapecity/wijmo.styles/wijmo.css"
+
+import * as wjGrid from "@grapecity/wijmo.grid"
+
+new wjGrid.FlexGrid("#app",{
+  itemsSource:getData()
+})
+
+
+function getData(){
+  let countries ="US,UK,India,China,Japan,Italy".split(",");
+  let data=[];
+  for(let i=0;i<12;i++){
+    data.push({
+      id:i,
+      country:countries[i%countries.length],
+      sales:Math.random()*10000,
+      expenses:Math.random()*1000,
+      active:i%2==0
+    });
+    
+  }
+  return data;
+}
